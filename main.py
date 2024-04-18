4baabbc6fd21e96223e2a71d9e2d73db5b0cec63
Szabolcs Jenei <jenei1337@gmail.com>
2024-04-18 17:43:50 +0200

    finished code

---
+++ main.py
@@ -0,0 +1,120 @@
+from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
+import json
+
+
+# Generate Password
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+
+    password_list = password_letters + password_numbers + password_symbols
+
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
+
+# Save Password, find email and password data in data.json
+def find_password():
+    try:
+        with open("data.json", "r") as file:
+            data = json.load(file)
+            found_data = data[f"{website_entry.get()}"]
+            messagebox.showinfo(title=f"{website_entry.get()}", message=f"Email: {found_data["email"]}\nPassword: {found_data["password"]}")
+    except FileNotFoundError:
+        messagebox.showerror(title="Error", message="No Data File Found")
+    except KeyError:
+        messagebox.showerror(title="Error", message="No details for the website exists")
+
+
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showerror(title="Oops", message="Please don't leave any fields empty!")
+    else:
+        try:
+            with open("data.json", "r") as f:
+                # Reading old data
+                data = json.load(f)
+        except FileNotFoundError:
+            with open("data.json", "w") as f:
+                json.dump(new_data, f, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_data)
+
+            with open("data.json", "w") as f:
+                # Saving updated data
+                json.dump(data, f, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+            website_entry.focus()
+
+
+# GUI
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=200, height=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(column=1, row=0)
+
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+website_entry = Entry(width=30)
+website_entry.grid(column=1, row=1, sticky="W")
+website_entry.focus()
+
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+
+email_entry = Entry(width=35)
+email_entry.grid(column=1, row=2, columnspan=2, sticky="EW")
+email_entry.insert(0, "your@email.com")
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+
+password_entry = Entry(width=30)
+password_entry.grid(column=1, row=3, sticky="W")
+
+password_button = Button(text="Generate Password", command=generate_password)
+password_button.grid(column=2, row=3, sticky="EW")
+
+add_button = Button(text="Add", width=36, command=save)
+add_button.grid(column=1, row=4, columnspan=2, sticky="EW")
+
+search_button = Button(text="Search",command=find_password)
+search_button.grid(column=2, row=1, sticky="EW")
+
+window.mainloop()
